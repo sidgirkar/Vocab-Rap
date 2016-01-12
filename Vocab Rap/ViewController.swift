5aa98ad42b5f74465d78bf50243fabba4e014b59
Sid Girkar <sidgirkar@sids-mbp>
2016-01-12 15:01:10 -0800

    Initial Commit

---
+++ Vocab Rap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vocab Rap
+//
+//  Created by Sid Girkar on 1/12/16.
+//  Copyright © 2016 Sid Girkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
